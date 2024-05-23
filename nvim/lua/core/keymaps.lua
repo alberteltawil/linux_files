ac7028b9c5c0b7cd5ff6e429ce864dfd3f049654
aeltawil <aeltawil@globeandmail.com>
2024-05-23 02:17:49 -0400

    added nvim lua files

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- vim.opt.backspace = '4'
+vim.opt.scroll = 5
+vim.opt.number = true
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
