cee409ded1d7f80a96de3eb99ce11ebb45333e10
aeltawil <aeltawil@globeandmail.com>
2024-07-04 01:53:40 -0400

    fix: updated battery.sh file to reflect 100% battery level and adjusted colors

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+BAT=$(acpi -b | grep -E -o '[0-9]+%' | tr -d '%')
 
 # Full and short texts
-echo "Battery: $BAT"
-echo "BAT: $BAT"
+echo "Battery: $BAT%"
+echo "BAT: $BAT%"
 
 # Set urgent flag below 10% or use orange below 20%
-[ ${BAT%?} -le 10 ] && exit 33
-[ ${BAT%?} -le 20 ] && echo "#FF8000"
+if [ "$BAT" -le 15 ]; then
+    echo "#FF0000"
+    #exit 33
+elif [ "$BAT" -le 30 ]; then
+    echo "#FF8000"
+elif [ "$BAT" -ge 100 ]; then
+    echo "#00FF00" # Green background for 100%
+fi
 
 exit 0
